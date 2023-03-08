97dd5fb7a3dc58c56f762cdb1ff90198712211af
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-08 15:47:21 +0900

    Update index.mjs

--- action/index.mjs
+++ action/index.mjs
@@ -1,5 +1,7 @@
-import { S3LocalSync, S3ProdStyleSync, S3ProdScriptSync } from "./dist/index.mjs"
+import { CompileStyle, CompileScript, S3LocalSync, S3ProdStyleSync, S3ProdScriptSync } from "./dist/index.mjs"
 
 new S3LocalSync;
 new S3ProdStyleSync;
 new S3ProdScriptSync;
+new CompileStyle;
+new CompileScript;
