a01c8d2010ea749acc3973fa256b5589bfd4d3bf
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-10 00:19:31 +0900

    Create index.ts

---
+++ scripts/1/index.ts
@@ -0,0 +1,9 @@
+import { DocContent } from "./class"
+
+export default {
+    css: [1,],
+    js: [2,],
+    scope: "cec073ceb46482ef596df4c8724c4134",
+    type: "class",
+    component: DocContent,
+}
