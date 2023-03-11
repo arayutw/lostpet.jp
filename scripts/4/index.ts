aebd23979cae691330a4a840a4b18f71e5d93978
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-11 12:35:49 +0900

    Create index.ts

---
+++ scripts/4/index.ts
@@ -0,0 +1,10 @@
+import { Module } from "../component/module"
+import { UIMain } from "./class"
+
+export default {
+    css: [1003,],
+    svg: [3, 4, 5, 13,],
+    scope: "cec073ceb46482ef596df4c8724c4134",
+    type: "class",
+    component: UIMain,
+} as Module
