0047d4af8fbcddf1621fcd8d93b33a8210c36192
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-11 12:35:42 +0900

    Create index.ts

---
+++ scripts/3/index.ts
@@ -0,0 +1,9 @@
+import { Module } from "../component/module"
+import { UIFooter } from "./class"
+
+export default {
+    css: [1002, 105,],
+    scope: "cec073ceb46482ef596df4c8724c4134",
+    type: "class",
+    component: UIFooter,
+} as Module
