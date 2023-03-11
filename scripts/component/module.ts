205a6283839e845cba4c3ba433ca69335eae69ae
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-11 12:36:41 +0900

    Create module.ts

---
+++ scripts/component/module.ts
@@ -0,0 +1,13 @@
+import { StyleIds } from "../script/css"
+import { ScriptIds } from "../script/js"
+import { SVGIds } from "../script/svg"
+import { Component } from "./class"
+
+export type Module = {
+    css?: StyleIds
+    js?: ScriptIds
+    svg?: SVGIds
+    scope: "cec073ceb46482ef596df4c8724c4134",
+    type: "class"
+    component: typeof Component
+}
