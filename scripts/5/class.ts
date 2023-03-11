49da367d95628450312c3a0563c40dec3a686a6f
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-11 12:35:50 +0900

    Create class.ts

---
+++ scripts/5/class.ts
@@ -0,0 +1,25 @@
+import { Column2Document } from "../1/class";
+import { Column2DocumentContent } from "../1/interface";
+import { Component, InitOptions } from "../component";
+import { ContentData } from "../script/content";
+
+export class UITermsContent extends Component implements Column2DocumentContent {
+    id!: number
+    declare P: Column2Document
+
+    constructor(options: InitOptions) {
+        super({
+            id: options.id,
+            P: options.P,
+        });
+    }
+
+    create(data: ContentData & {
+        body?: any
+    }) {
+        this.P.ui.main.update({
+            background: 1,
+            body: data.body,
+        });
+    }
+}
