0da56e0d61222bbe1978a96a37945bc1ba1cb20f
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-11 12:35:35 +0900

    Update index.ts

--- scripts/2/index.ts
+++ scripts/2/index.ts
@@ -1,3 +1,4 @@
+import { Module } from "../component/module"
 import { UIHeader } from "./class"
 
 export default {
@@ -5,4 +6,4 @@ export default {
     scope: "cec073ceb46482ef596df4c8724c4134",
     type: "class",
     component: UIHeader,
-}
+} as Module
