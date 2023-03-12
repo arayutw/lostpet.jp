020deefcb87ab89f9f6cb17e3736ff475dd93840
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-12 16:21:27 +0900

    Update index.ts

--- scripts/5/index.ts
+++ scripts/5/index.ts
@@ -1,7 +1,10 @@
 import { Module } from "../component/module"
 import { UITermsContent } from "./class"
 
+export const dependenciesStyleIds = [104, 1004, 1005,]
+
 export default {
+    css: dependenciesStyleIds,
     scope: "cec073ceb46482ef596df4c8724c4134",
     type: "class",
     component: UITermsContent,
