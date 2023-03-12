98a0f47b0f7a6d24dd952b6eac54eb95145cb263
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-12 16:21:40 +0900

    Create index.ts

---
+++ scripts/8/index.ts
@@ -0,0 +1,21 @@
+import { Module } from "../component/module"
+import { Win } from "../script/window"
+import { Popup } from "./class"
+import { PopupItem } from "./item";
+
+export const dependenciesStyleIds = [106, 1006,]
+
+export default {
+    css: dependenciesStyleIds,
+    scope: "cec073ceb46482ef596df4c8724c4134",
+    type: "class",
+    component: [
+        Popup,
+        PopupItem,
+    ],
+    run: (win: Win, module: Module) => {
+        win.caches.popup = new (module.component[0] as typeof Popup)({
+            P: win,
+        });
+    },
+} as Module
