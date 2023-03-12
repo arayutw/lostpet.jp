5aeb88b15a2e8d4e6a473442c417aed3a0c8e433
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-12 16:21:14 +0900

    Update index.ts

--- scripts/3/index.ts
+++ scripts/3/index.ts
@@ -1,8 +1,10 @@
 import { Module } from "../component/module"
 import { UIFooter } from "./class"
 
+export const dependenciesStyleIds = [1002, 105,]
+
 export default {
-    css: [1002, 105,],
+    css: dependenciesStyleIds,
     scope: "cec073ceb46482ef596df4c8724c4134",
     type: "class",
     component: UIFooter,
