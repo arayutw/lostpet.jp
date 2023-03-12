89a673aff2e295921b1e49ae406b4bec1267e1c5
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-12 23:24:37 +0900

    Update class.ts

--- scripts/4/class.ts
+++ scripts/4/class.ts
@@ -1,5 +1,6 @@
 import { dependenciesStyleIds } from ".";
 import { Component, InitOptions } from "../component";
+import { ElementUnit } from "../script/element";
 
 type UpdateOptions = {
     background: number
@@ -29,6 +30,85 @@ export class UIMain extends Component {
             this.create();
 
         }
+
+        const elements = this.element.getElementsByClassName("c3b1b2");
+
+        win.popup.attach((elements[elements.length - 1].getElementsByClassName("c3b1b2a")[0] as HTMLAnchorElement), (event: Event) => {
+            const win = this.window;
+
+            const targetE = event.currentTarget!;
+
+            win.svg.load([
+                18, 17, 5,
+            ])
+                .then((svgUnits: ElementUnit[]) => {
+                    const currentValue = localStorage.getItem("t") || "0";
+
+                    win.popup.create({
+                        element: {
+                            attribute: {
+                                class: "c7",
+                            },
+                            children: ([
+                                [0, "自動判定", 0,],
+                                [1, "ライト", 1,],
+                                [2, "ダーク", 2,],
+                            ] as Array<[number, string, 0 | 1 | 2]>).map(([svgIndex, text, value]) => {
+                                return {
+                                    attribute: {
+                                        class: "c7i"
+                                    },
+                                    children: {
+                                        attribute: {
+                                            class: "a2 c7a c7ai" + (value === parseInt(currentValue, 10) ? " c7s" : "") + " hb2",
+                                        },
+                                        children: [
+                                            win.element.create(svgUnits[svgIndex], {
+                                                attribute: {
+                                                    height: "1em",
+                                                    width: "1em",
+                                                },
+                                                children: {
+                                                    attribute: {
+                                                        class: "c7p",
+                                                    },
+                                                },
+                                            }),
+                                            text,
+                                        ],
+                                        on: {
+                                            click: [
+                                                () => {
+                                                    const win = this.window;
+
+                                                    win.popup.close({
+                                                        annotation: 1,
+                                                    });
+
+                                                    localStorage[(!value ? "remove" : "set") + "Item"]("t", value);
+
+                                                    win.colorScheme.update();
+                                                },
+                                                {
+                                                    passive: true,
+                                                },
+                                            ],
+                                        },
+                                        tagName: "a",
+                                    },
+                                    tagName: "div",
+                                };
+                            }),
+                            tagName: "div",
+                        },
+                        id: "a",
+                        P: this,
+                        target: targetE,
+                        type: "menu",
+                    });
+                });
+
+        });
 
         this.window.css.attach(this, dependenciesStyleIds);
 
@@ -68,9 +148,26 @@ export class UIMain extends Component {
             return aE;
         };
 
+        const createSideContainer = (title: string, border: boolean, items: HTMLElement[]): HTMLDivElement => {
+            const containerE = document.createElement("div");
+            containerE.className = "c3b1b" + (border ? " c3b1c" : "");
+
+            const headingE = document.createElement("h5");
+            headingE.className = "c3b1b1";
+            headingE.textContent = title;
+
+            const UlE = document.createElement("ul");
+            UlE.className = "c3b1b2";
+            UlE.append(...items);
+
+            containerE.append(headingE, UlE);
+
+            return containerE;
+        }
+
         sideInnerDivE.append(
             sideHeadingE,
-            this.createSideContainer("検索", false, ([
+            createSideContainer("検索", false, ([
                 ["迷子", "lost", 1,],
                 ["保護", "find", 2,],
             ] as Array<[string, string, number]>).map(([text, slug, type,]) => {
@@ -80,16 +177,16 @@ export class UIMain extends Component {
                 aE.textContent = text;
                 return aE;
             })),
-            this.createSideContainer("お役立ち", false, ([
+            createSideContainer("お役立ち", false, ([
                 ["掲載", "/search/new", 4,],
                 ["ポスター", "/poster", 3,],
             ] as Array<[string, string, number]>).map(func1)),
-            this.createSideContainer("案内", false, ([
+            createSideContainer("案内", false, ([
                 ["利用規約", "/terms", 6,],
                 ["プライバシーポリシー", "/privacy", 7,],
                 ["問い合わせ", "/contact", 8,],
             ] as Array<[string, string, number]>).map(func1)),
-            this.createSideContainer("環境設定", false, ([
+            createSideContainer("環境設定", false, ([
                 ["カラーモード", 5,],
             ] as Array<[string, number]>).map(([text, svg,]) => {
                 const json2node = this.window.element;
@@ -112,22 +209,6 @@ export class UIMain extends Component {
         );
 
         containerE.append(mainE, sideE);
-    }
-
-    private createSideContainer(title: string, border: boolean, items: HTMLElement[]): HTMLDivElement {
-        const containerE = document.createElement("div");
-        containerE.className = "c3b1b";
-
-        const headingE = document.createElement("h5");
-        headingE.className = "c3b1b1";
-
-        const UlE = document.createElement("ul");
-        UlE.className = "c3b1b2";
-        UlE.append(...items);
-
-        containerE.append(headingE, UlE);
-
-        return containerE;
     }
 
     update(options: UpdateOptions): void {
