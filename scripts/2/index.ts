d14b0261935750c9c4073b2befaea11eb9cc5363
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-12 16:20:48 +0900

    Update index.ts

--- scripts/2/index.ts
+++ scripts/2/index.ts
@@ -1,8 +1,10 @@
 import { Module } from "../component/module"
 import { UIHeader } from "./class"
 
+export const dependenciesStyleIds = [1001, 101, 102,];
+
 export default {
-    css: [1001, 101, 102,],
+    css: dependenciesStyleIds,
     scope: "cec073ceb46482ef596df4c8724c4134",
     type: "class",
     component: UIHeader,
