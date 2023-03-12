e93fd1003319f46c9bfc39a073655e0b5f5c61c5
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-12 16:21:00 +0900

    Update index.ts

--- scripts/1/index.ts
+++ scripts/1/index.ts
@@ -1,9 +1,12 @@
-import { DocContent } from "./class"
+import { Module } from "../component/module"
+import { Column2Document } from "./class"
+
+export const dependenciesStyleIds = [1,];
 
 export default {
-    css: [1,],
-    js: [2,],
+    css: dependenciesStyleIds,
+    js: [2, 3, 4,],
     scope: "cec073ceb46482ef596df4c8724c4134",
     type: "class",
-    component: DocContent,
-}
+    component: Column2Document,
+} as Module
