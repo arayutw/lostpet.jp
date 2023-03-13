8430c9db9c4be70c7d4626ed6fed188f95ef7a8b
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-13 13:38:34 +0900

    Create index.ts

---
+++ scripts/12/index.ts
@@ -0,0 +1,8 @@
+import { Module } from "../component/module"
+import { Recaptcha } from "./class"
+
+export default {
+    scope: "cec073ceb46482ef596df4c8724c4134",
+    type: "class",
+    component: Recaptcha,
+} as Module
