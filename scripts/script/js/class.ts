db7ae4371d124505f350d3b7259562c19a25dce5
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-13 13:37:51 +0900

    Update class.ts

--- scripts/script/js/class.ts
+++ scripts/script/js/class.ts
@@ -29,61 +29,66 @@ export class JS extends Component {
                 } else {
                     import("/scripts/" + id + ".js?v=" + this.window.version)
                         .then((response) => {
-                            const module = response.default;
-                            const promises: Array<any> = [module,];
+                            if (this.S) {
+                                const module = response.default;
+                                const promises: Array<any> = [module,];
 
-                            if (module) {
-                                const scriptIds: Array<number> | undefined = module.js;
-                                const styleIds: Array<number> | undefined = module.css;
-                                const svgIds: Array<number> | undefined = module.svg;
+                                if (module) {
+                                    const scriptIds: Array<number> | undefined = module.js;
+                                    const styleIds: Array<number> | undefined = module.css;
+                                    const svgIds: Array<number> | undefined = module.svg;
 
-                                scriptIds?.forEach((id) => promises.push(this.load(id)));
-                                if (styleIds) promises.push(this.window.css.load(styleIds));
-                                if (svgIds) promises.push(this.window.svg.load(svgIds));
-                            }
-
-                            return Promise.all(promises);
-                        })
-                        .then((res) => {
-                            const promises: Array<any> = [];
-                            const module = res[0] as Module;
-
-                            if (module) {
-                                let component: any;
-                                const type = module.type;
-
-                                if ("cec073ceb46482ef596df4c8724c4134" === module.scope) {
-                                    if ("class" === type) {
-                                        let components = module.component;
-                                        if (!Array.isArray(components)) components = [components];
-                                        component = this.caches[id] = components[0];
-
-                                        components.forEach((component: ExtendsTarget) => {
-                                            this.window.factory.create(component);
-                                        });
-                                    } else if ("function" === type || "object" === type) {
-                                        component = this.caches[id] = module.component;
-                                    } else {
-                                        component = 1;
-                                    }
-
-                                    promises.push(component);
-
-                                    if (module.run) {
-                                        promises.push(module.run(this.window, module));
-                                    }
-
-                                } else {
-                                    const constructor = component = module;
-                                    promises.push(this.caches[id] = constructor ? constructor : module);
+                                    scriptIds?.forEach((id) => promises.push(this.load(id)));
+                                    if (styleIds) promises.push(this.window.css.load(styleIds));
+                                    if (svgIds) promises.push(this.window.svg.load(svgIds));
                                 }
-                            }
 
-                            return Promise.all(promises);
+                                return Promise.all(promises);
+                            }
                         })
                         .then((res) => {
                             if (this.S) {
-                                resolve(res[0]);
+                                const promises: Array<any> = [];
+                                const module = res![0] as Module;
+
+                                if (module) {
+                                    let component: any;
+                                    const type = module.type;
+
+                                    if ("cec073ceb46482ef596df4c8724c4134" === module.scope) {
+                                        if ("class" === type) {
+                                            let components = module.component;
+                                            if (!Array.isArray(components)) components = [components];
+                                            component = this.caches[id] = components[0];
+
+                                            components.forEach((component: ExtendsTarget) => {
+                                                this.window.factory.create(component);
+                                            });
+                                        } else if ("function" === type || "object" === type) {
+                                            component = this.caches[id] = module.component;
+                                        } else {
+                                            component = 1;
+                                        }
+
+                                        promises.push(component);
+
+                                        if (module.run) {
+                                            promises.push(module.run(this.window, module));
+                                        }
+
+                                    } else {
+                                        const constructor = component = module;
+                                        promises.push(this.caches[id] = constructor ? constructor : module);
+                                    }
+                                }
+
+                                return Promise.all(promises);
+                            }
+
+                        })
+                        .then((res) => {
+                            if (this.S) {
+                                resolve(res![0]);
                             }
                         })
                         .catch((err) => {
