48e241a82ac3570c6e401e7070d2a2a70caba075
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-14 12:35:10 +0900

    Create index.ts

---
+++ scripts/14/index.ts
@@ -0,0 +1,9 @@
+import { Module } from "../component/module"
+import { EmailInput } from "./class"
+
+export default {
+    js: [13],
+    scope: "cec073ceb46482ef596df4c8724c4134",
+    type: "class",
+    component: EmailInput,
+} as Module
