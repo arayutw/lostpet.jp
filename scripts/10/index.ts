8dbaf4c4fe9d8448939f5a234cee5448eada50db
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-14 12:34:55 +0900

    Update index.ts

--- scripts/10/index.ts
+++ scripts/10/index.ts
@@ -1,11 +1,17 @@
 import { Module } from "../component/module"
 import { UIContactContent } from "./class"
 
-export const dependenciesStyleIds = [108, 109, 1009,]
+export const dependenciesStyleIds = [
+    102,
+    108,
+    109,
+    110,
+    111,
+    1009,]
 
 export default {
     css: dependenciesStyleIds,
-    js: [12],
+    js: [12, 14, 15,],
     scope: "cec073ceb46482ef596df4c8724c4134",
     type: "class",
     component: UIContactContent,
