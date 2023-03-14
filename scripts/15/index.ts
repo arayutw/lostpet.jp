7e899677a599cf7f461c09f0d6a8a2c2a13483fb
arayutw <125749298+arayutw@users.noreply.github.com>
2023-03-14 12:35:16 +0900

    Create index.ts

---
+++ scripts/15/index.ts
@@ -0,0 +1,9 @@
+import { Module } from "../component/module"
+import { TextInput } from "./class"
+
+export default {
+    js: [13],
+    scope: "cec073ceb46482ef596df4c8724c4134",
+    type: "class",
+    component: TextInput,
+} as Module
